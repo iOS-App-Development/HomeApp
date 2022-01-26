7b70f62fe35216aa1fdc9a3d9dad69c304a5f029
Muzammil Peer <muzammilpeer987@gmail.com>
2022-01-27 02:22:57 +0400

    sample api called with test in respective module

--- HomeAppiOS/HomeAppiOS/ViewController.swift
+++ HomeAppiOS/HomeAppiOS/ViewController.swift
@@ -6,14 +6,38 @@
 //
 
 import UIKit
+import HomeFeature
+import CoreModule
+import HTTPClientModule
+import APIClientModule
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.test_endToEndTestServerGETFruits()
     }
 
+    private func ephemeralClient(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+//        trackForMemoryLeaks(client, file: file, line: line)
+        return client
+    }
 
+    func test_endToEndTestServerGETFruits() {
+        let service = HomeService.init(httpClient: ephemeralClient(), baseURL: URL.init(string: "https://api.themoviedb.org/3/")!)
+        service.getMoviePopularList { resultObjects in
+            switch resultObjects {
+            case let .success(fruits)?:
+                print("how many objects %d",fruits.first?.originalTitle)
+            case let .failure(error)?:
+                print("failed:\(error)")
+
+            default:
+                print("failed")
+            }
+        }
+    }
 }
 
