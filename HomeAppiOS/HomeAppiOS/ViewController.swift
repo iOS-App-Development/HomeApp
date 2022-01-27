32e3be56c70d2a34dc308331cf1ddaf707b49637
Muzammil Peer <muzammilpeer987@gmail.com>
2022-01-27 13:55:16 +0400

    first screen added and load with api via module without with builder

--- HomeAppiOS/HomeAppiOS/ViewController.swift
+++ HomeAppiOS/HomeAppiOS/ViewController.swift
@@ -16,7 +16,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        self.test_endToEndTestServerGETFruits()
+//        self.test_endToEndTestServerGETFruits()
+        
+        
+    }
+    
+    func buildHomeModule()
+    {
+        let homeService = HomeService.init(httpClient: ephemeralClient(), baseURL: URL.init(string: "https://api.themoviedb.org/3/")!)
+        let accountService = AccountService.init(httpClient: ephemeralClient(), baseURL: URL.init(string: "https://api.themoviedb.org/3/")!)
+
+        let homeModule:HomeFeature = HomeFeature.init(settings: Settings.init(), homeServices: homeService, accountServices: accountService, userInfoProvider: UserInfoProvider.init())
+        homeModule.start(on: self.navigationController!)
     }
 
     private func ephemeralClient(file: StaticString = #filePath, line: UInt = #line) -> HTTPClient {
@@ -25,19 +36,24 @@ class ViewController: UIViewController {
         return client
     }
 
-    func test_endToEndTestServerGETFruits() {
-        let service = HomeService.init(httpClient: ephemeralClient(), baseURL: URL.init(string: "https://api.themoviedb.org/3/")!)
-        service.getMoviePopularList { resultObjects in
-            switch resultObjects {
-            case let .success(fruits)?:
-                print("how many objects %d",fruits.first?.originalTitle)
-            case let .failure(error)?:
-                print("failed:\(error)")
-
-            default:
-                print("failed")
-            }
-        }
+//    func test_endToEndTestServerGETFruits() {
+//        let service = HomeService.init(httpClient: ephemeralClient(), baseURL: URL.init(string: "https://api.themoviedb.org/3/")!)
+//        service.getMoviePopularList { resultObjects in
+//            switch resultObjects {
+//            case let .success(fruits)?:
+//                print("how many objects %d",fruits.first?.originalTitle)
+//            case let .failure(error)?:
+//                print("failed:\(error)")
+//
+//            default:
+//                print("failed")
+//            }
+//        }
+//    }
+    
+    @IBAction func actionHomeScreen(_ sender: Any) {
+        self.buildHomeModule()
     }
+    
 }
 
